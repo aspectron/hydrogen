131e282f09595f9915f655d894e0608d97529fed
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2012-09-18 12:26:03 -0400

    Working on in-thread (async) implementation of window functions.

--- src/gui.windows.hpp
+++ src/gui.windows.hpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <vector>
 
+#include "async_queue.hpp"
+#include "events.hpp"
+
 namespace aspect
 {
 	namespace gui 
@@ -27,6 +30,17 @@ namespace aspect
 
 			public:
 
+				V8_DECLARE_CLASS_BINDER(window);
+
+				typedef struct _creation_args
+				{
+					uint32_t width, height, bpp, style;
+					std::string caption;
+				} creation_args;
+
+				void test_function_binding(void) { printf("TEST FUNCTION BINDING INVOKED!\n"); }
+
+
 				operator HWND () const { return hwnd_; }
 				static window *pwnd_from_hwnd(HWND hwnd)
 				{
@@ -37,189 +51,72 @@ namespace aspect
 #endif
 				}
 
-				window(video_mode mode, const std::string& caption, unsigned long requested_style) :
-				  hwnd_(NULL),
-				  style_(requested_style),
-				  fullscreen_(false)
-				  {
-					  // Compute position and size
-					  int left   = (GetDeviceCaps(GetDC(NULL), HORZRES) - mode.width)  / 2;
-					  int top    = (GetDeviceCaps(GetDC(NULL), VERTRES) - mode.height) / 2;
-					  int width  = width_ = mode.width;
-					  int height = height_ = mode.height;
-
-					  // Choose the window style according to the Style parameter
-					  DWORD style = WS_VISIBLE;
-					  if (style_ == AWS_NONE)
-					  {
-						  style |= WS_POPUP;
-					  }
-					  else
-					  {
-						  if (style_ & AWS_TITLEBAR) style |= WS_CAPTION | WS_MINIMIZEBOX;
-						  if (style_ & AWS_RESIZE)   style |= WS_THICKFRAME | WS_MAXIMIZEBOX;
-						  if (style_ & AWS_CLOSE)    style |= WS_SYSMENU;
-					  }
-
-					  // In windowed mode, adjust width and height so that window will have the requested client area
-					  if (!(style_ & AWS_FULLSCREEN))
-					  {
-						  RECT rect = {0, 0, width, height};
-						  AdjustWindowRect(&rect, style, false);
-						  width  = rect.right - rect.left;
-						  height = rect.bottom - rect.top;
-					  }
-
-#if 0
-					  wchar_t wcs_caption[256];
-					  int nb_chars = MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, caption.c_str(), static_cast<int>(caption.size()), wcs_caption, sizeof(wcs_caption) / sizeof(*wcs_caption));
-					  wcs_caption[nb_chars] = L'\0';
-					  hwnd_ = CreateWindowW(L"jsx", wcs_caption, style, left, top, width, height, NULL, NULL, GetModuleHandle(NULL), this);
-#else
-
-style =  WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_VISIBLE;
-
-					  hwnd_ = CreateWindowA("jsx_generic", caption.c_str(), style, left, top, width, height, NULL, NULL, GetModuleHandle(NULL), this);
-#endif				  
-
-					  // Switch to fullscreen if requested
-					  if (style_ & AWS_FULLSCREEN)
-						 switch_to_fullscreen(mode);
-
-					  // Get the actual size of the window, which can be smaller even after the call to AdjustWindowRect
-					  // This happens when the window is bigger than the desktop
-					  RECT actual_rect;
-					  GetClientRect(*this, &actual_rect);
-					  width_  = actual_rect.right - actual_rect.left;
-					  height_ = actual_rect.bottom - actual_rect.top;
-				  }
-
-				  virtual ~window()
-				  {
-					  destroy();
-				  }
-
-				  void destroy(void)
-				  {
-					  if(hwnd_)
-					  {
-						  ::DestroyWindow(hwnd_);
-					  }
-
-					  if(fullscreen_)
-					  {
-						  ChangeDisplaySettings(NULL,0);
-						  show_mouse_cursor(true);
-					  }
-				  }
-
-				  void show_mouse_cursor(bool show)
-				  {
-					  if(show)
-						  cursor_ = LoadCursor(NULL,IDC_ARROW);
-					  else
-						  cursor_ = NULL;
-
-					  ::SetCursor(cursor_);
-				  }
-
-				  void process_events(void)
-				  {
-					  MSG msg;
-					  //					  while (GetMessage(&msg, hwnd_, 0, 0, PM_REMOVE))
-					  while (PeekMessage(&msg, hwnd_, 0, 0, PM_REMOVE))
-					  {
-						  TranslateMessage(&msg);
-						  DispatchMessage(&msg);
-					  }
-				  }
-
-				  void process_events_blocking(void)
-				  {
-					  MSG msg;
-					  while (GetMessage(&msg, NULL, 0, 0))
-					  //while (PeekMessage(&msg, hwnd_, 0, 0, PM_REMOVE))
-					  {
-						  TranslateMessage(&msg);
-						  DispatchMessage(&msg);
-					  }
-				  }
-
+				window(const creation_args *);
+				virtual ~window();
+				void create_window_impl(const creation_args*);//video_mode mode, const std::string& caption, unsigned long requested_style);
+				void destroy_window(void);
+				void destroy_window_impl(void);
+				void show_mouse_cursor(bool show);
+				void process_event(UINT message, WPARAM wparam, LPARAM lparam);
+				void show(bool visible);
+				void switch_to_fullscreen(const video_mode& mode);
 
 // 				  void set_position(int left, int top)
 // 				  {
 // 					  SetWindowPos(*this, NULL, left, top, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 // 				  }
 
-				  void show(bool visible)
-				  {
-					  ShowWindow(*this,visible ? SW_SHOW : SW_HIDE);
-				  }
+				void process_events(void);
+				void process_events_blocking(void);
+		};
 
-				  void switch_to_fullscreen(const video_mode& mode)
-				  {
-					  DEVMODE DevMode;
-					  DevMode.dmSize       = sizeof(DEVMODE);
-					  DevMode.dmPelsWidth  = mode.width;
-					  DevMode.dmPelsHeight = mode.height;
-					  DevMode.dmBitsPerPel = mode.bpp;
-					  DevMode.dmFields     = DM_PELSWIDTH | DM_PELSHEIGHT | DM_BITSPERPEL;
+		class windows_thread
+		{
+			public:
 
-					  // Apply fullscreen mode
-					  if (ChangeDisplaySettings(&DevMode, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL)
-					  {
-						  std::cerr << "Failed to change display mode for fullscreen" << std::endl;
-						  return;
-					  }
+				void hello_world(void) { aspect::trace("hello hydrogen!"); }
 
-					  // Change window style (no border, no titlebar, ...)
-					  SetWindowLong(*this, GWL_STYLE,   WS_POPUP);
-					  SetWindowLong(*this, GWL_EXSTYLE, WS_EX_APPWINDOW);
+				windows_thread();
+				virtual ~windows_thread();
 
-					  // And resize it so that it fits the entire screen
-					  SetWindowPos(*this, HWND_TOP, 0, 0, mode.width, mode.height, SWP_FRAMECHANGED);
-					  ShowWindow(*this, SW_SHOW);
+				static windows_thread *global() { return global_; }
 
-					  // SetPixelFormat can fail (really ?) if window style doesn't contain these flags
-					  long style = GetWindowLong(*this, GWL_STYLE);
-					  SetWindowLong(*this, GWL_STYLE, style | WS_CLIPCHILDREN | WS_CLIPSIBLINGS);
+				/// Callback function to schedule in Berkelium
+				typedef boost::function<void ()> callback;
 
-					  fullscreen_ = true;
-				  }
+				/// Schedule function call in the windows thread
+				static bool schedule(callback cb);
 
-				  void process_event(UINT message, WPARAM wparam, LPARAM lparam)
-				  {
-					  if(hwnd_ == NULL)
-						  return;
+				// Check that caller in the windows thread
+				static bool is_berkelium_thread()
+				{
+					return boost::this_thread::get_id() == global_->thread_.get_id();
+				}
 
-					  switch(message)
-					  {
+				void main();
+				boost::thread thread_;
 
-						case WM_SIZE:
-							{
-								RECT rect;
-								GetClientRect(*this, &rect);
-								width_ = rect.right - rect.left;
-								height_ = rect.bottom - rect.top;
+				class main_loop;
+				boost::scoped_ptr<main_loop> main_loop_;
 
-							} break;
+				boost::scoped_ptr<async_queue> task_queue_;
 
-						case WM_CLOSE:
-							{
-								if(style_ & AWS_APPWINDOW)
-								{
-									set_terminating();
-									PostQuitMessage(true);
-								}
-							} break;
-					  }
-				  }
+
+			private:
+
+				static windows_thread *global_;
+
 		};
 
 		void HYDROGEN_API init(HINSTANCE hInstance);
 		void HYDROGEN_API cleanup(void);
 	}
 }
+
+#define WEAK_CLASS_TYPE aspect::gui::window
+#define WEAK_CLASS_NAME window
+#include <v8/juice/WeakJSClassCreator-Decl.h>
+
 
 #endif // OS(WINDOWS)
 
--- src/hydrogen.cpp
+++ src/hydrogen.cpp
@@ -4,39 +4,79 @@
 
 using namespace v8;
 using namespace v8::juice;
-/*
-V8_IMPLEMENT_CLASS_BINDER(aspect::test_class, aspect_test_class);
 
-DECLARE_LIBRARY_ENTRYPOINTS(test_install, test_uninstall);
+V8_IMPLEMENT_CLASS_BINDER(aspect::gui::window, aspect_window);
 
-void test_install(Handle<Object> target)
+DECLARE_LIBRARY_ENTRYPOINTS(hydrogen_install, hydrogen_uninstall);
+
+void hydrogen_install(Handle<Object> target)
 {
 //	HandleScope scope;
+	using namespace aspect::gui;
 
-	ClassBinder<aspect::test_class> *binder = new ClassBinder<aspect::test_class>(target);
-	V8_SET_CLASS_BINDER(aspect::test_class, binder);
-	(*binder)
-		.BindMemFunc<void, &aspect::test_class::test_function_binding>("test_function_binding")
+	ClassBinder<aspect::gui::window> *binder_window = new ClassBinder<aspect::gui::window>(target);
+	V8_SET_CLASS_BINDER(aspect::gui::window, binder_window);
+	(*binder_window)
+		.BindMemFunc<void, &window::test_function_binding>("test_function_binding")
+		.BindMemFunc<void, &window::destroy_window>("destroy")
 		.Seal();
+
+	// ---
+
+#if OS(WINDOWS)
+	aspect::gui::init((HINSTANCE)GetModuleHandle(NULL));
+#else
+	aspect::gui::init();
+#endif
 }
 
-void test_uninstall(Handle<Object> target) 
+void hydrogen_uninstall(Handle<Object> target) 
 {
-	V8_DESTROY_CLASS_BINDER(aspect::test_class);
+	V8_DESTROY_CLASS_BINDER(aspect::gui::window);
+
+	aspect::gui::cleanup();
 }
 
 namespace v8 { namespace juice {
 
-aspect::test_class * WeakJSClassCreatorOps<aspect::test_class>::Ctor( v8::Arguments const & argv, std::string & exceptionText )
+aspect::gui::window* WeakJSClassCreatorOps<aspect::gui::window>::Ctor( v8::Arguments const & args, std::string & exceptionText )
 {
-	return new aspect::test_class();
+	using namespace aspect;
+	using namespace aspect::gui;
+
+//		video_mode mode(1280,720,32);
+	if(!args.Length())
+		throw new std::runtime_error("Window constructor requires configuration object as an argument");
+
+	window::creation_args ca;
+
+	Handle<Object> o = args[0]->ToObject();
+
+	ca.width = convert::JSToUInt32(o->Get(String::New("width")));
+	if(!ca.width || ca.width > 1024*10)
+		ca.width = 640;
+	ca.height = convert::JSToUInt32(o->Get(String::New("height")));
+	if(!ca.height || ca.height > 1024*10)
+		ca.height = 480;
+	ca.bpp = convert::JSToUInt32(o->Get(String::New("bpp")));
+	if(!ca.bpp)
+		ca.bpp = 32;
+
+	ca.caption = convert::JSToStdString(o->Get(String::New("caption")));
+
+	ca.style = convert::JSToUInt32(o->Get(String::New("style")));
+	if(!ca.style)
+		ca.style = AWS_TITLEBAR | AWS_RESIZE | AWS_CLOSE | AWS_APPWINDOW;
+
+
+
+	return new aspect::gui::window(&ca);
 }
 
-void WeakJSClassCreatorOps<aspect::test_class>::Dtor( aspect::test_class *o )
+void WeakJSClassCreatorOps<aspect::gui::window>::Dtor( aspect::gui::window *o )
 {
 	delete o;
 }
 
 }} // ::v8::juice
 
-*/
