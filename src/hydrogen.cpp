4a34514e53ee722b4804337913fae0cc5a9f2bdf
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2013-01-11 20:43:11 -0500

    Integrated image library.

--- src/hydrogen.cpp
+++ src/hydrogen.cpp
@@ -9,6 +9,7 @@ DECLARE_LIBRARY_ENTRYPOINTS(hydrogen_install, hydrogen_uninstall);
 void hydrogen_install(Handle<Object> target)
 {
 //	HandleScope scope;
+	aspect::image::init();
 
 	ClassBinder<aspect::gl::engine> *binder_engine = new ClassBinder<aspect::gl::engine>(target);
 	V8_SET_CLASS_BINDER(aspect::gl::engine, binder_engine);
@@ -74,5 +75,7 @@ void hydrogen_install(Handle<Object> target)
 void hydrogen_uninstall(Handle<Object> target) 
 {
 	V8_DESTROY_CLASS_BINDER(aspect::gl::engine);
+
+	aspect::image::cleanup();
 }
 
